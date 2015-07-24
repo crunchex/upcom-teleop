c69a9a8eafc7859a93fcc7b89d6a6c37ded6a6c7
Mike Lewis <mike@updroid.com>
2015-07-24 09:04:42 -0700

    tool: removed editor code block from build script

--- tool/build.sh
+++ tool/build.sh
@@ -11,18 +11,6 @@ command -v pub >/dev/null 2>&1 || {
 	exit 1;
 }
 
-# Get extra assets
-cd $TOPDIR/web
-if [ ! -d "src-min-noconflict" ]; then
-	git clone --quiet https://github.com/ajaxorg/ace-builds.git
-	cd ace-builds
-	# make sure we're using package 03.03.15
-	git checkout --quiet beb9ff68e397b4dcaa1d40f79651a063fc917736
-	mv src-min-noconflict ../src-min-noconflict
-fi
-cd $TOPDIR/web
-rm -rf ace-builds
-
 # Build front-end -> build/web
 cd $TOPDIR
 pub get
