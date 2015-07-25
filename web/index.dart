9e0712c33bd6c0e3f766bf17dc2a2ece25bd11de
Mike Lewis <mike@updroid.com>
2015-07-24 17:24:43 -0700

    web: fixed asset paths

--- web/index.dart
+++ web/index.dart
@@ -4,7 +4,7 @@ import 'teleop.dart';
 void main() {
   ScriptElement teleopJs = new ScriptElement()
   ..type = 'text/javascript'
-  ..src = 'http://localhost:12060/upcom-teleop/teleop.js';
+  ..src = 'http://localhost:12060/tabs/upcom-teleop/teleop.js';
   document.body.children.add(teleopJs);
 
   teleopJs.onLoad.first.then((_) {
