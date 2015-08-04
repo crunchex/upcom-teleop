160312671c352b00ad7f6abd77e65d11d846be45
Mike Lewis <mike@updroid.com>
2015-08-04 15:14:28 -0700

    tool: added debug mode to build script

--- tool/build.sh
+++ tool/build.sh
@@ -4,6 +4,26 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TOPDIR=$DIR/..
 
+function usage {
+    echo "usage: build [[[-d ] | [-h]]"
+}
+
+# process args
+debug=0
+while [ "$1" != "" ]; do
+    case $1 in
+        -d | --debug )       	shift
+                                debug=1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
 # Check dependencies
 command -v pub >/dev/null 2>&1 || {
 	echo "FAIL";
@@ -14,12 +34,20 @@ command -v pub >/dev/null 2>&1 || {
 # Build front-end -> build/web
 cd $TOPDIR
 pub get
-pub build
+if [ $debug == 1 ]; then
+	pub build --mode=debug
+else
+	pub build
+fi
 
 # Build back-end -> build/bin
 BUILDBIN=$TOPDIR/build/bin
 mkdir -p $BUILDBIN
-dart2js --output-type=dart --categories=Server --minify -o $BUILDBIN/main.dart $TOPDIR/bin/main.dart
+if [ $debug == 1 ]; then
+	dart2js --output-type=dart --categories=Server -o $BUILDBIN/main.dart $TOPDIR/bin/main.dart
+else
+	dart2js --output-type=dart --categories=Server --minify -o $BUILDBIN/main.dart $TOPDIR/bin/main.dart
+fi
 rm -rf $BUILDBIN/main.dart.deps
 
 # Copy over tabinfo.json -> build/bin
